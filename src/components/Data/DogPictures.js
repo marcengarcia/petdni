2bebd0448f9387bcc21ed13f645d3e6b7ffc5917
marcengarcia <marcengarcia@gmail.com>
2023-03-14 01:29:19 -0300

    removed image

--- src/components/Data/DogPictures.js
+++ src/components/Data/DogPictures.js
@@ -3,21 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleArrowLeft, faCircleArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 
-const slides = [
-    '../images/molly1.jpg',
-    '../images/molly2.jpg',
-    '../images/molly3.jpg',
-]
-
-
-
-
 const DogPictures = () => {
     return (
         <div className='carrousel'>
-            {/* <img src={require('../images/molly1.jpg')} className='profile-pictures' alt='Profile pictures for the dog' /> */}
-
-            <ImageSlider slides={slides}/>
             <div className='dog-name'>Molly</div>
             <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow-back" />
             <FontAwesomeIcon icon={faCircleArrowRight} className="arrow-forward" />
