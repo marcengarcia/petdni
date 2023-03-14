ec658d5436a13ec2b607a0950b69a1145d155d2c
marcengarcia <marcengarcia@gmail.com>
2023-03-14 00:16:58 -0300

    added carrousel component

--- src/components/Data/DogPictures.js
+++ src/components/Data/DogPictures.js
@@ -1,10 +1,14 @@
 import './DogPictures.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleArrowLeft, faCircleArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const DogPictures = () => {
     return (
         <div className='carrousel'>
-            <img src='../images/IMG-20201015-WA0043.jpg' className='profile-pictures' alt='Profile pictures for the dog'/>
+            <img src='../images/IMG-20201015-WA0043.jpg' className='profile-pictures' alt='Profile pictures for the dog' />
             <div className='dog-name'>Molly</div>
+            <FontAwesomeIcon icon={faCircleArrowLeft} className="arrow-back" />
+            <FontAwesomeIcon icon={faCircleArrowRight} className="arrow-forward" />
         </div>
     )
 }
