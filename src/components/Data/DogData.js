2830212384b274c5fa74d3951e6ddfa41d6aa8b6
marcengarcia <marcengarcia@gmail.com>
2023-03-14 00:08:31 -0300

    added carrousel component

--- src/components/Data/DogData.js
+++ src/components/Data/DogData.js
@@ -4,6 +4,10 @@ import './DogData.css'
 const DogData = () => {
     return (
         <div className='accordion'>
+            {/* TODO:
+            necesito poder tener el primer item seleccionado, si agrego un checked al estado necesito 2 clicks para expandir las secciones.
+            
+            */}
             <input type="radio" name="accordion" id="first"/>
             <label for="first">datos del perruno</label>
             <div className='content'>datos del perruno
--- src/components/Data/DogPictures.js
+++ src/components/Data/DogPictures.js
@@ -1,7 +1,12 @@
 import './DogPictures.css'
 
 const DogPictures = () => {
-    return <div className='dog-picture-container'>Pictures aca</div>
+    return (
+        <div className='carrousel'>
+            <img src='../images/IMG-20201015-WA0043.jpg' className='profile-pictures' alt='Profile pictures for the dog'/>
+            <div className='dog-name'>Molly</div>
+        </div>
+    )
 }
 
 export default DogPictures
