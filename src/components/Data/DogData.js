807adc89884b657557a9d8bddc42bc557c3a17e2
marcengarcia <marcengarcia@gmail.com>
2023-03-13 23:10:24 -0300

    enhanced 1 click menu display

--- src/components/Data/DogData.js
+++ src/components/Data/DogData.js
@@ -4,7 +4,7 @@ import './DogData.css'
 const DogData = () => {
     return (
         <div className='accordion'>
-            <input type="radio" name="accordion" id="first" checked></input>
+            <input type="radio" name="accordion" id="first"></input>
             <label for="first">datos del perruno</label>
             <div className='content'>datos del perruno
             texto textotexto textotexto textotexto textotexto textotexto textotexto textotexto textotexto textotexto textotexto texto 
