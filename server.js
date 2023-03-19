f8a1f8c6ee17add6d58daddf023fbe39db3f3513
marcengarcia <marcengarcia@gmail.com>
2023-03-19 17:18:35 -0300

    added server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true)
     var filename = "." + q.pathname
-    if (filename == './') { filename = './index' }
+    if (filename == './') { filename = './public/index' }
     filename = filename + ".html"
     fs.readFile(filename, function (err, data) {
         if (err) {
