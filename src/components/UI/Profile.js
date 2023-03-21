61c3f6c6dbfdcea28a36c1d5d4d861d1b5bacae0
marcengarcia <marcengarcia@gmail.com>
2023-03-21 00:19:09 -0300

    finished profile component

--- src/components/UI/Profile.js
+++ src/components/UI/Profile.js
@@ -1,8 +1,4 @@
 import "./Profile.css"
-import DogPictures from '../Data/DogPictures';
-import DogData from '../Data/DogData';
-import HumanData from '../Data/HumanData';
-import OtherData from '../Data/OtherData';
 import Footer from '../Data/Footer';
 import Header from '../Data/Header';
 import Slider from "../Data/Slider";
@@ -13,64 +9,67 @@ const Profile = () => {
         <div className="card">
 
             <div className="header"><Header /></div>
-            <div className="image-area"> <Slider />AGREGAR IMAGEN</div>
+            <div className="carrousel"> <Slider />AGREGAR IMAGEN</div>
             <div className='accordion'>
-                <input type="radio" name="accordion" id="first" />
-                <label for="first">datos del perruno</label>
-                <div className='content'>
-                    <table>
-                        <tr>
-                            <td class="highlight"><div></div></td>
-                            <td>Nombre:</td>
-                            <td className='value'><input type="text" /></td>
-                            <td class="highlight"><div></div></td>
-                        </tr>
-                        <tr>
-                            <td class="highlight"><div></div></td>
-                            <td>Edad:</td>
-                            <td className='value'><input type="text" /></td>
-                            <td class="highlight"><div></div></td>
-                        </tr>
-                        <tr>
-                            <td class="highlight"><div></div></td>
-                            <td>Raza:</td>
-                            <td className='value'><input type="text" /></td>
-                            <td class="highlight"><div></div></td>
-                        </tr>
-                        <tr>
-                            <td class="highlight"><div></div></td>
-                            <td>Sexo:</td>
-                            <td className='value'><input type="text" /></td>
-                            <td class="highlight"><div></div></td>
-                        </tr>
-                        <tr>
-                            <td class="highlight"><div></div></td>
-                            <td>Castrada:</td>
-                            <td className='value'><input type="text" /></td>
-                            <td class="highlight"><div></div></td>
-                        </tr>
-                        <tr>
-                            <td class="highlight"><div></div></td>
-                            <td>Vacunas al Dia:</td>
-                            <td className='value'><input type="text" /></td>
-                            <td class="highlight"><div></div></td>
-                        </tr>
-                        <tr>
-                            <td class="highlight"><div></div></td>
-                            <td>Chippeada:</td>
-                            <td className='value'><input type="text" /></td>
-                            <td class="highlight"><div></div></td>
-                        </tr>
-                        <tr>
-                            <td class="highlight"><div></div></td>
-                            <td>Otro:</td>
-                            <td className='value'><input type="text" /></td>
-                            <td class="highlight"><div></div></td>
-                        </tr>
-                    </table>
-                </div >
+                <div className="dog-data">
+                    <input type="radio" name="accordion" id="first" />
+                    <label for="first">datos del perruno</label>
+                    <div className='dog-info-table'>
+                        <table>
+                            <tr>
+                                <td class="highlight"><div></div></td>
+                                <td>Nombre:</td>
+                                <td className='value'><input type="text" /></td>
+                                <td class="highlight"><div></div></td>
+                            </tr>
+                            <tr>
+                                <td class="highlight"><div></div></td>
+                                <td>Edad:</td>
+                                <td className='value'><input type="text" /></td>
+                                <td class="highlight"><div></div></td>
+                            </tr>
+                            <tr>
+                                <td class="highlight"><div></div></td>
+                                <td>Raza:</td>
+                                <td className='value'><input type="text" /></td>
+                                <td class="highlight"><div></div></td>
+                            </tr>
+                            <tr>
+                                <td class="highlight"><div></div></td>
+                                <td>Sexo:</td>
+                                <td className='value'><input type="text" /></td>
+                                <td class="highlight"><div></div></td>
+                            </tr>
+                            <tr>
+                                <td class="highlight"><div></div></td>
+                                <td>Castrada:</td>
+                                <td className='value'><input type="text" /></td>
+                                <td class="highlight"><div></div></td>
+                            </tr>
+                            <tr>
+                                <td class="highlight"><div></div></td>
+                                <td>Vacunas al Dia:</td>
+                                <td className='value'><input type="text" /></td>
+                                <td class="highlight"><div></div></td>
+                            </tr>
+                            <tr>
+                                <td class="highlight"><div></div></td>
+                                <td>Chippeada:</td>
+                                <td className='value'><input type="text" /></td>
+                                <td class="highlight"><div></div></td>
+                            </tr>
+                            <tr>
+                                <td class="highlight"><div></div></td>
+                                <td>Otro:</td>
+                                <td className='value'><input type="text" /></td>
+                                <td class="highlight"><div></div></td>
+                            </tr>
+                        </table>
+                    </div >
 
-                <div className='accordion'>
+
+                </div>
+                <div className='human-data'>
                     <input type="radio" name="accordion" id="second" />
                     <label for="second">datos del humano</label>
                     <div className='content'>
@@ -115,7 +114,7 @@ const Profile = () => {
                     </div>
                 </div>
 
-                <div className='accordion'>
+                <div className='other-data'>
                     <input type="radio" name="accordion" id="third" />
                     <label for="third">otros datos</label>
                     <div className='content'>
@@ -133,11 +132,11 @@ const Profile = () => {
                         </table>
                     </div>
                 </div>
-
             </div >
-            <div className="footer"> <Footer /></div>
-        </div>
-    )
+            <div className="footer"> <Footer />
+            </div>
+            </div>
+            )
 }
 
-export default Profile;
+            export default Profile;
