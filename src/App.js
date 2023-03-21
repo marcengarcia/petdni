24866017d9ba0c2e9adb28d93e21013d4312e640
marcengarcia <marcengarcia@gmail.com>
2023-03-20 22:06:27 -0300

    added routing to APP.js for 3 main pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes as Switch, Route, Link } from 'react-router-dom'
+
 import Card from './components/UI/Card';
 import Login from './components/UI/Login';
 import Register from './components/UI/Register';
 
 
 const App = () => {
-  
+
 
   return (
-    <div>
-      <Card/>
-    </div>
+    <Router>
+      <div className='container'>
+        <Switch>
+          <Route path='/' Component={Card} exact/>
+          <Route path='/login' Component={Login} exact/>
+          <Route path='/Register' Component={Register} exact/>
+        </Switch>
+
+      </div>
+    </Router>
   )
 }
 
