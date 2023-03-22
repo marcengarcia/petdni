9345407c08fea37dd35926b3245011c05d71b11f
marcengarcia <marcengarcia@gmail.com>
2023-03-21 22:10:31 -0300

    added db creation logic to server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+const mysql = require('mysql')
+
+
+
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "pet_dni",
+})
+
+conn.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to the MySql");
+    conn.query("CREATE DATABASE IF NOT EXISTS pet_dni", function (err, result) {
+        if (err) throw err;
+        console.log("db created")
+    })
+
+    })
+
+
 
 app.get('/', (req, res) => {
     res.send('Holis, soy tu server =)')
 })
 
-app.listen(PORT || 3001, () => {
+app.listen(PORT, () => {
     console.log('running on port: ' + PORT)
 })
