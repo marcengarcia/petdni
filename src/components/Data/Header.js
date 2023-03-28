2711d3142c3138653a76d627eff633d10f49ace2
marcengarcia <marcengarcia@gmail.com>
2023-03-28 18:10:50 -0300

    changed navbar to be more simple (active indicator missing)

--- src/components/Data/Header.js
+++ src/components/Data/Header.js
@@ -14,35 +14,33 @@ const Header = () => {
     };
     return (
         <nav>
-            <div className='navigation' >
-                <ul>
-                    <li onClick={() => handleIconClick('faPaw')} className={activeIcon === 'faPaw' ? 'active' : ''} > 
-                        <Link to='/pets'>
-                            <span className='icon'>
-                                <FontAwesomeIcon icon={faPaw} />
-                            </span>
-                            <span className='text'>Mascotas</span>
-                        </Link>
-                    </li>
-                    <li onClick={() => handleIconClick('faIdCard')} className={activeIcon === 'faIdCard' ? 'active' : ''}>
-                        <Link to='/profile'>
-                            <span className='icon'>
-                                <FontAwesomeIcon icon={faIdCard} />
-                            </span>
-                            <span className='text'>Perfil</span>
-                        </Link>
-                    </li>
-                    <li onClick={() => handleIconClick('faRightFromBracket')} className={activeIcon === 'faRightFromBracket' ? 'active' : ''}>
-                        <Link to='/login'>
-                            <span className='icon'>
-                                <FontAwesomeIcon icon={faRightFromBracket} />
-                            </span>
-                            <span className='text'>Salir</span>
-                        </Link>
-                    </li>
-                    <div className='indicator'></div>
-                </ul>
-            </div>
+            <ul>
+                <li onClick={() => handleIconClick('faPaw')} className={activeIcon === 'faPaw' ? 'active' : ''} >
+                    <Link to='/pets'>
+                        <span className='icon'>
+                            <FontAwesomeIcon icon={faPaw} />
+                        </span>
+                        <span className='text'>Mascotas</span>
+                    </Link>
+                </li>
+                <li onClick={() => handleIconClick('faIdCard')} className={activeIcon === 'faIdCard' ? 'active' : ''}>
+                    <Link to='/profile'>
+                        <span className='icon'>
+                            <FontAwesomeIcon icon={faIdCard} />
+                        </span>
+                        <span className='text'>Perfil</span>
+                    </Link>
+                </li>
+                <li onClick={() => handleIconClick('faRightFromBracket')} className={activeIcon === 'faRightFromBracket' ? 'active' : ''}>
+                    <Link to='/login'>
+                        <span className='icon'>
+                            <FontAwesomeIcon icon={faRightFromBracket} />
+                        </span>
+                        <span className='text'>Salir</span>
+                    </Link>
+                </li>
+                <div className='indicator'></div>
+            </ul>
         </nav>
     )
 }
--- src/components/UI/Login.js
+++ src/components/UI/Login.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
 
+
     return (
         <div className='login-container'>
             <div className='login-wrapper'>
