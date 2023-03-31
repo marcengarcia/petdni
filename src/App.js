992970120d97d7b0d88e4583f0a473a948e22095
marcengarcia <marcengarcia@gmail.com>
2023-03-31 19:19:42 -0300

    dinamically added placeholder info to dog data component on profile

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import PrivateRounter from './components/utils/PrivateRouter'
 
 
 const App = () => {
+
   let dogData = {
     name: 'Mollys',
     age: '2 años',
@@ -21,30 +22,32 @@ const App = () => {
     chipped: 'Si',
     allergies: 'No',
     other: 'No'
-}
-let humanData = {
-  name: 'Marcelus',
-  phone: '+54 123456789',
-  instagram: '@marcengarcia',
-  twitter: '@marcengarcia',
-  Facebook: '@marcengarcia',
-  other: ''
-}
+  }
 
-let otherData = {
-  otherInfo: 'Aca iria un text area read only con info que los dueños crean que es importante y no esta en los campos de las tablas',
-}
+  let humanData = {
+    name: 'Marcelus',
+    phone: '+54 123456789',
+    instagram: '@marcengarcia',
+    twitter: '@marcengarcia',
+    Facebook: '@marcengarcia',
+    other: ''
+  }
+
+  let otherData = {
+    otherInfo: 'Aca iria un text area read only con info que los dueños crean que es importante y no esta en los campos de las tablas',
+  }
+  
   return (
     <Router>
-        <Routes>
-          <Route element={<PrivateRounter/>}>
-            <Route path='/profile' element={<Profile dogData={dogData} humanData={humanData} otherData={otherData}/>} exact />
-            <Route path='/pets' element={<Pets dogData={dogData} humanData={humanData} otherData={otherData}/>} exact />
-          </Route>
-          <Route path='/' element={<Landing/>} exact />
-          <Route path='/login' element={<Login/>} exact />
-          <Route path='/register' element={<Register/>} exact />
-        </Routes>
+      <Routes>
+        <Route element={<PrivateRounter />}>
+          <Route path='/profile' element={<Profile dogData={dogData} humanData={humanData} otherData={otherData} />} exact />
+          <Route path='/pets' element={<Pets dogData={dogData} humanData={humanData} otherData={otherData} />} exact />
+        </Route>
+        <Route path='/' element={<Landing />} exact />
+        <Route path='/login' element={<Login />} exact />
+        <Route path='/register' element={<Register />} exact />
+      </Routes>
     </Router>
   )
 }
