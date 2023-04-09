41969ef31d15d967802f1905e4d6036a68c0a85a
marcengarcia <marcengarcia@gmail.com>
2023-04-08 23:53:16 -0300

    added user table logic

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+const db = require('./models')
 
 app.get('/', (req, res) => {
     res.send('Holis, soy tu server =)')
 })
-
-app.listen(PORT || 3001, () => {
-    console.log('running on port: ' + PORT)
+db.sequelize.sync().then((req) => {
+    app.listen(PORT || 3001, () => {
+        console.log('running on port: ' + PORT)
+    })
 })
