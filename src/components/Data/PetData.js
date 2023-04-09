71c586dc03f9375af8834aa2634449a75c578dbd
marcengarcia <marcengarcia@gmail.com>
2023-04-09 00:54:54 -0300

    renamed components from DOG to PET

---
+++ src/components/Data/PetData.js
@@ -0,0 +1,57 @@
+import './PetData.css'
+
+const DogData = (props) => {
+
+    return (
+        <div className='accordion'>
+            <input type="radio" name="accordion" id="first" defaultChecked />
+            <label for="first">datos del perruno</label>
+            <div className='content'>
+                <table>
+                    <tr>
+                        <td>Nombre:</td>
+                        <td className='value'>{props.dogData?.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Edad:</td>
+                        <td className='value'>{props.dogData?.age}</td>
+                    </tr>
+                    <tr>
+                        <td>Raza:</td>
+                        <td className='value'>{props.dogData?.breed}</td>
+                    </tr>
+                    <tr>
+                        <td>Sexo:</td>
+                        <td className='value'>{props.dogData?.sex}</td>
+                    </tr>
+                    <tr>
+                        <td>Castrada:</td>
+                        <td className='value'>{props.dogData?.neutered}</td>
+                    </tr>
+                    <tr>
+                        <td>Vacunas al Dia:</td>
+                        <td className='value'>{props.dogData?.vaxxed}</td>
+                    </tr>
+                    <tr>
+                        <td>Chippeada:</td>
+                        <td className='value'>{props.dogData?.chipped}</td>
+                    </tr>
+                    <tr>
+                        <td>Alergias:</td>
+                        <td className='value'>{props.dogData?.allergies}</td>
+                    </tr>
+                    <tr>
+                        <td>Dieta:</td>
+                        <td className='value'>{props.dogData?.diet}</td>
+                    </tr>
+                    <tr>
+                        <td>Otro:</td>
+                        <td className='value'>{props.dogData?.other}</td>
+                    </tr>
+                </table>
+            </div >
+        </div >
+    )
+}
+
+export default DogData
