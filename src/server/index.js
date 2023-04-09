67bbdbd897d63c6b43d9691a003c8785532d4726
marcengarcia <marcengarcia@gmail.com>
2023-04-08 23:39:41 -0300

    added server and initial dependencies

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
+app.get('/', (req, res) => {
+    res.send('Holis, soy tu server =)')
+})
+
 app.listen(PORT || 3001, () => {
     console.log('running on port: ' + PORT)
 })
